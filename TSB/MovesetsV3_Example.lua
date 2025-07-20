d156b0d58044c89706d6e3d6985c7c0f7d70e63f
softbf395 <130502864+softbf395@users.noreply.github.com>
2025-07-19 22:15:57 -0500

    Update MovesetsV3_Example.lua

--- TSB/MovesetsV3_Example.lua
+++ TSB/MovesetsV3_Example.lua
@@ -9,6 +9,8 @@ ms:ChrSel("Bye!", 6005761509, "Testing Notices", function()
     local music=ms:SFX(true, "https://raw.githubusercontent.com/softbf395/Utilities/refs/heads/main/TSB/V2/NoCopyrightSounds%20-%20Boom%20Kitty%20%26%20Waterflame%20-%20Citadel%20Bass%20House%20NCS%20-%20Copyright%20Free%20Music.mp3", "music/TSB/Template")
 music:Play()
 
+-- ms:ImportVFX("VFX/example/VFX", "https://example.com/example.rbxm") -- import a Part that holds VFX
+
 ms:Ult("Ult Name", Color3.fromRGB(255,255,255), function() print("Ult Activated!") end)
 ms:Create(5, --[[move ID, 1 to 9]] function() end, --[[Callback for when used]] 2, --[[Cooldown for custom moves]] "Move Name")
 
